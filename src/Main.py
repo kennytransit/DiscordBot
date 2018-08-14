1f7d83a9e2eee8cefa482ce6640de02a893a86fa
brendancaywood <brendan.m.caywood@gmail.com>
2018-08-14 16:29:24 -0400

    Simple bot

---
+++ src/Main.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext.commands import Bot
+import discord.utils
+
+
+
+BOT_PREFIX = ('!bren')
+bot = Bot(command_prefix=BOT_PREFIX)
+tallydict = {}
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('---------')
+
+@bot.command(name='addTally',
+                description="Adds a tally",
+                brief="get roasted",
+                aliases=['tally', 'addtally', 'Addtally'],
+                pass_context=True)
+async def addTally(ctx, desUser: str, num: int):
+    if desUser in tallydict:
+        tallydict[desUser] += num
+        msg = desUser + ' now has ' + str(tallydict[desUser]) + ' tallies.'
+        await ctx.send(msg)
+    else:
+        tallydict[desUser] = num
+        await ctx.send(desUser + ' has recieved their first tally. They now have ' + str(tallydict[desUser]) + ' tallies.')
+
+
+
+
+
+
+
